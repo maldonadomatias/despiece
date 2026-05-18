c42f961cda3e6632a8ea843df9f29afe503cacd7
Matias Agustin Maldonado <matiasagustinmaldonado@192.168.1.180>
2026-05-18 15:32:52 -0300

    fix(backend): exclude dist/ from Jest test discovery

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -14,5 +14,6 @@ export default {
     ],
   },
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
 };
